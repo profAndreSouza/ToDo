6d565eecf9bdf37753bbef984f4db6d0519d99cf
profAndreSouza <andre.souza@sp.senai.br>
2025-03-26 22:21:00 -0300

    doc: updated

--- obj/Debug/net9.0/ToDoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ToDoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da7fd9a730351fe8e0ab410cc745ab0c58c3daa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+130775af654fbbf7fccbba1ede7e972989a3c3e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
